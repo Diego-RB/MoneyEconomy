0d1a1818c047acada4e088ebc9fdd686479a9ecf
Diego Barbosa <diego.barbosa@iatec.com>
2021-11-18 22:53:15 -0300

    Gastos e alteração no menu

--- cadastroGastos.c
+++ cadastroGastos.c
@@ -1,2 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+
+void gastos(){
+    char nome[256][256] = {""};
+  
+    for (int i = 0; i < 2; i++)
+    {
+        fflush(stdin);
+        printf("Digite seu nome: ");
+        gets(nome[i]);
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+     printf("O nome armazenado foi: %s\n", nome[i]);
+    }
+
+    getch();
+}
+
+
+
 
--- main.c
+++ main.c
@@ -14,35 +14,36 @@ int main (void){
    SetConsoleOutputCP(65001);
    printf("BEM VINDO!");
 
-   // menu:
-   // opcao = menu();
-   // system("cls");
+   menu:
+   opcao = menu();
+   system("cls");
 
-   // switch(opcao){
-   //    case 0:
-   //       system("cls");
-   //       printf("\n\tTchau, obrigado pela preferencia!\n");
-   //       break;
+   switch(opcao){
+      case 0:
+         system("cls");
+         printf("\n\tTchau, obrigado pela preferencia!\n");
+         break;
 
-   //    case 1:
-   //       printf("Cadastrar gastos\n"),
-   //       system("pause");
-   //       goto menu;
+      case 1:
+         //printf("Cadastrar gastos\n"),
+         gastos();
+         system("pause");
+         goto menu;
 
-   //    case 2:
-   //       printf("VERIFICAR GASTOS\n");
-   //       system("pause");
-   //       goto menu;
+      case 2:
+         printf("VERIFICAR GASTOS\n");
+         system("pause");
+         goto menu;
       
-   //    case 3:
-   //       printf("SOBRE NÓS\n");
-   //       system("pause");
-   //       goto menu;
+      case 3:
+         printf("SOBRE NÓS\n");
+         system("pause");
+         goto menu;
 
-   //    default:
-   //       printf("\n\tA apção selecionada não existe. Tente novamente!\n\n");
-   //       system("pause");
-   //       goto menu;
-   // }
+      default:
+         printf("\n\tA apção selecionada não existe. Tente novamente!\n\n");
+         system("pause");
+         goto menu;
+   }
     return 0;
 }
