d8c53194ce8cc842c7d3674465c2e4e8f87f85b5
Diego Barbosa <diego.barbosa@iatec.com>
2021-11-28 16:53:11 -0300

    Bem vindo add

--- sobre.c
+++ sobre.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 void Sobre (){
-         printf("A educação financeira é extremamente essencial para aumentar a qualidade de vida e esse é o nosso objetivo: facilitar a disciplina com os gastos dos usuários.\n Esse programa foi desenvolvido visando gerenciamento de gastos do usuário mediante ao salário cadastrado.\n Espera-se, que dessa forma, consigamos facilmente organizar o capital que entra, o que sai e o que sobra ou falta.\n O programa foi desenvolvido por três alunos no segundo semestre do curso de engenharia da computação: Wlliam Hideaki Herai, Diego Roela Barbosa e Aline de Oliveira Lima");
+        printf("\n\tSOBRE\n\n");
+        printf("\tA educação financeira é extremamente essencial para aumentar a qualidade de vida e esse é o nosso objetivo: facilitar a disciplina com os gastos dos usuários.\n\t Esse programa foi desenvolvido visando gerenciamento de gastos do usuário mediante ao salário cadastrado.\n\t Espera-se, que dessa forma, consigamos facilmente organizar o capital que entra, o que sai e o que sobra ou falta.\n\t O programa foi desenvolvido por três alunos no segundo semestre do curso de engenharia da computação:\n\t Wlliam Hideaki Herai, Diego Roela Barbosa e Aline de Oliveira Lima");
 }
